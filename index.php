681129d7a2769485dcc32c851df542587208442f
Mateus Roberto Algayer <mateusr.algayer@gmail.com>
2024-08-22 13:49:54 -0300

    alteração

--- index.php
+++ index.php
@@ -57,41 +57,18 @@ try {
     case $CAMINHO_BASE.'/usuario/inserir':
       UsuariosController::inserir();
       break;
-    //API
-    case $CAMINHO_BASE.'/admin/eventos/cadastro/form':
-      RotasAdmin::eventosForm();
     //---------------- Admin -----------------------------
     case $CAMINHO_BASE.'/admin/':
       RotasAdmin::dashboardView();
       break;
-    case $CAMINHO_BASE.'/api/admin/usuario/inserir':
-      UsuariosController::inserirAPI();
-      break;
-    //WEB
-    case $CAMINHO_BASE.'admin/eventos/alteracao/form':
+    case $CAMINHO_BASE.'/admin/eventos/cadastro/form':
       RotasAdmin::eventosForm();
-    case $CAMINHO_BASE.'/api/admin/usuario/alterar':
-      UsuariosController::alterarAPI();
-      break;
+    case $CAMINHO_BASE.'/admin/eventos/alteracao/form':
+      RotasAdmin::eventosForm();
     case $CAMINHO_BASE.'/admin/eventos/detalhes/form':
       RotasAdmin::eventosForm();
-    case $CAMINHO_BASE.'/api/admin/usuario/excluir':
-      UsuariosController::excluirAPI();
-      break;
     case $CAMINHO_BASE.'/admin/logout':
       AcessoController::fazLogout();
-    case $CAMINHO_BASE.'/web/admin/usuario/inserir':
-      UsuariosController::inserir();
-      break;
-    case $CAMINHO_BASE.'/web/admin/usuario/alterar':
-      UsuariosController::alterar();
-      break;
-    case $CAMINHO_BASE.'/web/admin/usuario/excluir':
-      UsuariosController::excluir();
-      break;
-    //TODO: AdminRotasController
-    case $CAMINHO_BASE.'/web/admin/eventos/alteracao/form':
-      // EventosController::alterar();
       break;
     case $CAMINHO_BASE.'/admin/eventos/inserir':
       EventosController::inserir();
