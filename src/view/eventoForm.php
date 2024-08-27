18be7ac2ab3a22a52afbfdb4b3a4a2a5a7d8cb89
Mateus Roberto Algayer <mateusr.algayer@gmail.com>
2024-08-27 19:05:08 -0300

    Correções

--- src/view/eventoForm.php
+++ src/view/eventoForm.php
@@ -5,11 +5,11 @@
   } else {
     $acao = 'inserir';
     $evento = [
-      'id'=> '',
-      'titulo'=> '',
-      'descricao'=> '',
-      'local'=> '',
-      'dataEvento'=> '',
+      'ID'=> '',
+      'TITULO'=> '',
+      'DESCRICAO'=> '',
+      'LOCAL'=> '',
+      'DATAEVENTO'=> '',
     ];
   }
 ?>
@@ -18,22 +18,22 @@
   <div class="login-container">
     <h2>Cadastrar evento</h2>
     <form  action="/gerenciadorEventos/admin/eventos/<?=$acao?>" method="post">
-      <input type="hidden" id="id" name="id" value="<?=$evento['id']?>" required>
+      <input type="hidden" id="id" name="id" value="<?=$evento['ID']?>" required>
       <div class="form-group">
         <label for="titulo">Título</label>
-        <input type="text" id="titulo" name="titulo" value="<?=$evento['titulo']?>" required>
+        <input type="text" id="titulo" name="titulo" value="<?=$evento['TITULO']?>" required>
       </div>
       <div class="form-group">
         <label for="descricao">Descrição</label>
-        <input type="text" id="descricao" name="descricao" value="<?=$evento['descricao']?>" required>
+        <input type="text" id="descricao" name="descricao" value="<?=$evento['DESCRICAO']?>" required>
       </div>
       <div class="form-group">
         <label for="local">Local</label>
-        <input type="text" id="local" name="local" value="<?=$evento['local']?>" required>
+        <input type="text" id="local" name="local" value="<?=$evento['LOCAL']?>" required>
       </div>
       <div class="form-group">
         <label for="dataEvento">Data</label>
-        <input type="date" id="dataEvento" name="dataEvento" value="<?=$evento['dataEvento']?>" required>
+        <input type="date" id="dataEvento" name="dataEvento" value="<?=$evento['DATAEVENTO']?>" required>
       </div>
       <div class="form-group">
         <button class="btn" type="submit">Salvar</button>
